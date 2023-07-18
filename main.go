5ff2baac2cddfbc7a374cc4b24cc25fba9fbfb6a
Moyo <31387340+MoyosoreCodes@users.noreply.github.com>
2023-07-18 23:26:41 +0100

    hash maps

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt" 
-	"os"
-	"strings"
 )
 
 func main() {
@@ -14,16 +12,26 @@ func main() {
 	// ! LOOPS
 	// i, arg := range os.Args[1:]
 	// similar to for index, item in
-	for i, arg := range os.Args[1:] {
-		fmt.Println(i, arg)
-	}
+	// for i, arg := range os.Args[1:] {
+	// 	fmt.Println(i, arg)
+	// }
 
 	// initialization; condition; increment
 	// The first element of os.Args, os.Args[0], is the name of the command its elf; 
-	for i := 0; i < len(os.Args); i++ {
-		fmt.Println(os.Args[i])
-	}
+	// for i := 0; i < len(os.Args); i++ {
+	// 	fmt.Println(os.Args[i])
+	// }
 
-	fmt.Println(strings.Join(os.Args[1:], " "))
-	fmt.Println(os.Args[1:][0])
+	// fmt.Println(strings.Join(os.Args[1:], " "))
+	// fmt.Println(os.Args[1:][0])
+
+	// maps
+	hashMap := make(map[string]string)
+	hashMap["foo"] = "bar"
+	fmt.Println(hashMap)
+
+	for key, value := range hashMap {
+		fmt.Println("key: ", key)
+		fmt.Println("value: ", value)
+	}
 }
