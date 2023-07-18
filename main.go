02b0493cd2feb3f7a5d50503b14567d16a39abee
Moyo <31387340+MoyosoreCodes@users.noreply.github.com>
2023-07-18 22:40:09 +0100

    added loops

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt" 
+	"os"
+)
 
 func main() {
+	// basic stuff
 	fmt.Println("Your lack of faith disturbs me")
+
+	
+	// ! LOOPS
+	// i, arg := range os.Args[1:]
+	// similar to for index, item in
+	for i, arg := range os.Args[1:] {
+		fmt.Println(i, arg)
+	}
+
+	// initialization; condition; increment
+	// The firs t element of os.Args, os.Args[0], is the name of the command its elf; 
+	for i := 0; i < len(os.Args); i++ {
+		fmt.Println(os.Args[i])
+	}
 }
