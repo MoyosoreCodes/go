b7495d91c62d1af75c0e7b87a4b2150a039a2dc5
Moyo <31387340+MoyosoreCodes@users.noreply.github.com>
2023-09-05 15:04:33 +0100

    advent of code day 2 part 1

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"sort"
 	"strconv"
-	"fmt"
+	"strings"
 )
 
 func check(e error) {
@@ -53,6 +54,63 @@ func getTopCalories(array []int, top int) []int {
 	return array[:top]
 }
 
+func getTotalGameScore(fileName string) int {
+	file, err := os.Open(fileName)
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	// scores := []int{}
+	currentScore := 0
+
+	// // player moves
+	opponent := map[string]string{
+		"A": "Rock",
+		"B": "Paper",
+		"C": "Scissors",
+	}
+	user := map[string]string{
+		"X": "Rock",
+		"Y": "Paper",
+		"Z": "Scissors",
+	}
+
+	// // points
+	round := map[string]int{
+		"win":  6,
+		"draw": 3,
+		"loss": 0,
+	}
+	shape := map[string]int{
+		"Rock":     1,
+		"Paper":    2,
+		"Scissors": 3,
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		moves := strings.Split(string(line), " ")
+		opponentsMove := shape[opponent[moves[0]]]
+		userMove := shape[user[moves[1]]]
+
+		// first player (opponent) win would 1
+		//  second player (user) would be 2
+		// tie would be zero
+		outcome := ((opponentsMove - userMove + 3) % 3)
+
+		currentScore += userMove
+		if outcome == 2 {
+			currentScore += round["win"]
+		} else if outcome == 1 {
+			currentScore += round["loss"]
+		} else if outcome == 0 {
+			currentScore += round["draw"]
+		}
+
+		return currentScore
+	}
+}
+
 func main() {
 	calories := getCaloriesTotal("day1.txt")
 	topThree := getTopCalories(calories, 3)
@@ -63,4 +121,7 @@ func main() {
 		sum += calory
 	}
 	fmt.Println("topThree total: ", sum)
+
+	scores := getTotalGameScore("day2.txt")
+	fmt.Println(scores)
 }
